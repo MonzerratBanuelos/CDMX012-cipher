eb595df6b2d5c91e244698faab6f5e2f8fb69e4e
MonzerratBanuelos <ambgmonze@gmail.com>
2022-01-24 00:55:41 -0600

    codigo al dia 24 Enero 2022

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
+  encode: function (mensaje,desplazamiento) {
+    var encoded = "";
+    for (let i = 0; i < mensaje.length; i++) {
+      let letraAscii = mensaje.charCodeAt(i);
+      if(letraAscii >= 65 && letraAscii <= 90){
+      encoded += String.fromCharCode(((letraAscii - 65 + desplazamiento) % 26) + 65);
+      }else{
+        encoded += String.fromCharCode(letraAscii);
+      }
+    } //console.log(encoded,"resultado")
+    return encoded;
+  } //,
+  //decode: function (mensaje,desplazamiento) {
+  //  var decoded= "";
+  //for (let i = 0; i < mensaje.length; i++) {
+  //  let letraAscii = mensaje.charCodeAt(i);
+  //  if(letraAscii >= 65 && letraAscii <= 90){
+  //  decoded += String.fromCharCode(((letraAscii - 65 - desplazamiento) % 26) + 65);
+  //  }else{
+  //    decoded += String.fromCharCode(letraAscii);
+  //  }
+  //} console.log(decoded,"resultado")
+  //return decoded;
+//}
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import cipher from './cipher.js';
+const clickSaludo = document.getElementById("clickSaludo")
+const titulo = document.getElementById("titulo")
+
+clickSaludo.addEventListener("click",function(){ 
+  titulo.innerHTML = cipher.saludar()
+});
 //AQUI SE CONFIGURA EL BOTON QUE MUESTRA PAGINA DE CIFRADO 
 let btnCifrator = document.getElementById("startCifrator"); 
 function showCipherPage(){
   let cipherPage = document.getElementById("cipherpage").style.display='block';
   document.getElementById("root").style.display="none";
+  return cipherPage
 }
   btnCifrator.addEventListener("click",showCipherPage);
   //AQUI SE CONFIGURA EL BOTON QUE MUESTRA PAGINA DE INICIO 
@@ -11,11 +18,12 @@ let btnReturn = document.getElementById("btnReturn");
 function backtoMenu(){
   let cipherPage = document.getElementById("root").style.display='block';
   document.getElementById("cipherpage").style.display="none";
+  return cipherPage
 }
   btnReturn.addEventListener("click",backtoMenu);
 //AQUI SE CONFIGURA LA PAGINA DE CIFRADO
   //donde se esta diciendo que encode llama a mensaje y desplazamiento 
-  function encode(mensaje,desplazamiento) {
+  /*function encode(mensaje,desplazamiento) {
     var encoded = "";
     for (let i = 0; i < mensaje.length; i++) {
       let letraAscii = mensaje.charCodeAt(i);
@@ -26,7 +34,7 @@ function backtoMenu(){
       }
     } console.log(encoded,"resultado")
     return encoded;
-  }
+  } */
 //AQUÍ SE CONFIGURA LA FORMULA DE DESCIFRADO  
  /* function decode(mensaje,desplazamiento) {
     let decoded = "";
@@ -45,14 +53,14 @@ function backtoMenu(){
   function showText(){
     let mensaje = document.getElementById("textToCipher").value
     //mensaje = mensaje.toUpperCase()
-    console.log(mensaje);
+   // console.log(mensaje);
     let desplazamiento = document.getElementById("displacement").value
     desplazamiento = parseInt(desplazamiento);
-    console.log(desplazamiento);
+    //console.log(desplazamiento);
     
     // decode(mensaje,desplazamiento);
     let mensajeCifrado = document.getElementById("cipherText");
-    mensajeCifrado.innerHTML=encode(mensaje,desplazamiento);
+    mensajeCifrado.innerHTML=cipher.encode(mensaje,desplazamiento);
     }
 btncifrar.addEventListener("click",showText);
 //btncifrar.addEventListener("click",encoded);
