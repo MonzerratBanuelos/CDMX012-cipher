0948e56064f705ae0b0dea1d62c07feee7e66a53
MonzerratBanuelos <ambgmonze@gmail.com>
2022-01-25 01:15:57 -0600

    Avances al dia 25 enero 2022

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,41 @@
 const cipher = {
-  encode: function (mensaje,desplazamiento) {
-    var encoded = "";
-    for (let i = 0; i < mensaje.length; i++) {
+  encode: function (offset,mensaje) {
+    if(typeof(mensaje) === "string" && typeof(offset) === "number"){
+      let encoded = "";
+      for (let i = 0; i < mensaje.length; i++) {
       let letraAscii = mensaje.charCodeAt(i);
       if(letraAscii >= 65 && letraAscii <= 90){
-      encoded += String.fromCharCode(((letraAscii - 65 + desplazamiento) % 26) + 65);
-      }else{
+        encoded += String.fromCharCode(((letraAscii - 65 + offset) % 26) + 65);
+      }else if (letraAscii >=97 && letraAscii <=122){
+        encoded += String.fromCharCode((letraAscii - 97 + offset) % 26 + 97);
+      }
+      else{
         encoded += String.fromCharCode(letraAscii);
       }
-    } //console.log(encoded,"resultado")
-    return encoded;
-  } //,
-  //decode: function (mensaje,desplazamiento) {
-  //  var decoded= "";
-  //for (let i = 0; i < mensaje.length; i++) {
-  //  let letraAscii = mensaje.charCodeAt(i);
-  //  if(letraAscii >= 65 && letraAscii <= 90){
-  //  decoded += String.fromCharCode(((letraAscii - 65 - desplazamiento) % 26) + 65);
-  //  }else{
-  //    decoded += String.fromCharCode(letraAscii);
-  //  }
-  //} console.log(decoded,"resultado")
-  //return decoded;
-//}
+      } return encoded;
+    } else {
+      throw new TypeError ("Tipo de dato invalido o inexistente")
+    }
+  },
+  decode: function (offset,mensaje) {
+    if(typeof(mensaje) === "string" && typeof(offset) === "number"){
+      var decoded= "";
+      for (let i = 0; i < mensaje.length; i++) {
+      let letraAscii = mensaje.charCodeAt(i);
+      if(letraAscii >= 65 && letraAscii <= 90){
+        decoded += String.fromCharCode((letraAscii - 90 - offset) % 26 + 90);
+      }else if (letraAscii >= 97 && letraAscii <= 122){
+        decoded += String.fromCharCode((letraAscii - 122 - offset) % 26 + 122);
+      }
+      else{
+        decoded += String.fromCharCode(letraAscii);
+      }
+    } //console.log(decoded,"resultado")
+    return decoded;
+    }else{
+      throw new TypeError ("tipo de dato invalido o inexistente")
+    }
+    
+}
 };
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import cipher from './cipher.js';
-const clickSaludo = document.getElementById("clickSaludo")
-const titulo = document.getElementById("titulo")
-
-clickSaludo.addEventListener("click",function(){ 
-  titulo.innerHTML = cipher.saludar()
-});
 //AQUI SE CONFIGURA EL BOTON QUE MUESTRA PAGINA DE CIFRADO 
 let btnCifrator = document.getElementById("startCifrator"); 
 function showCipherPage(){
@@ -13,59 +7,39 @@ function showCipherPage(){
   return cipherPage
 }
   btnCifrator.addEventListener("click",showCipherPage);
-  //AQUI SE CONFIGURA EL BOTON QUE MUESTRA PAGINA DE INICIO 
+
+
+//AQUI SE CONFIGURA EL BOTON QUE MUESTRA PAGINA DE INICIO 
 let btnReturn = document.getElementById("btnReturn"); 
 function backtoMenu(){
   let cipherPage = document.getElementById("root").style.display='block';
   document.getElementById("cipherpage").style.display="none";
+  document.getElementById("textToCipher").value="";
+  document.getElementById("cipherText").value="";
   return cipherPage
 }
   btnReturn.addEventListener("click",backtoMenu);
-//AQUI SE CONFIGURA LA PAGINA DE CIFRADO
-  //donde se esta diciendo que encode llama a mensaje y desplazamiento 
-  /*function encode(mensaje,desplazamiento) {
-    var encoded = "";
-    for (let i = 0; i < mensaje.length; i++) {
-      let letraAscii = mensaje.charCodeAt(i);
-      if(letraAscii >= 65 && letraAscii <= 90){
-      encoded += String.fromCharCode(((letraAscii - 65 + desplazamiento) % 26) + 65);
-      }else{
-        encoded += String.fromCharCode(letraAscii);
-      }
-    } console.log(encoded,"resultado")
-    return encoded;
-  } */
-//AQUÍ SE CONFIGURA LA FORMULA DE DESCIFRADO  
- /* function decode(mensaje,desplazamiento) {
-    let decoded = "";
-    for (let i = 0; i < mensaje.length; i++) {
-      let letraAscii = mensaje.charCodeAt(i);
-      if(letraAscii >= 65 && letraAscii <= 90){
-      decoded += String.fromCharCode(((letraAscii - 65 - desplazamiento) % 26) + 65);
-      }else{
-        decoded += String.fromCharCode(letraAscii);
-      }
-    } console.log(decoded,"resultado")
-    return decoded;
-  } */
-//AQUÍ SE CONFIGURA BOTÓN QUE MUESTRA TEXTO EN PANTALLA Y EN MAYUSCULAS 
+
+
+//AQUI SE CONFIGURA EL BOTON DE DESCIFRAR
+  let btndecode = document.getElementById("btn2");
+  function showDecode() {
+    let mensaje = document.getElementById("textToCipher").value
+    let offset = document.getElementById("offset").value
+    offset = parseInt(offset);
+    let mensajeCifrado = document.getElementById("cipherText");
+    mensajeCifrado.innerHTML=cipher.decode(offset,mensaje);
+  }
+  btndecode.addEventListener("click",showDecode);
+
+//AQUÍ SE CONFIGURA BOTÓN QUE MUESTRA TEXTO EN PANTALLA 
   let btncifrar = document.getElementById("btn1");
   function showText(){
     let mensaje = document.getElementById("textToCipher").value
-    //mensaje = mensaje.toUpperCase()
-   // console.log(mensaje);
-    let desplazamiento = document.getElementById("displacement").value
-    desplazamiento = parseInt(desplazamiento);
-    //console.log(desplazamiento);
-    
-    // decode(mensaje,desplazamiento);
+    let offset = document.getElementById("offset").value
+    offset = parseInt(offset);
     let mensajeCifrado = document.getElementById("cipherText");
-    mensajeCifrado.innerHTML=cipher.encode(mensaje,desplazamiento);
+    mensajeCifrado.innerHTML=cipher.encode(offset,mensaje);
     }
 btncifrar.addEventListener("click",showText);
 //btncifrar.addEventListener("click",encoded);
-//Lo de arriba es el manager que dice que al escuchar click muestre la función 
-
-  //PASOS POR HACER
-  //CONVERTIR A ASCII
-  //AUMENTAR EL DESPLAZAMIENTO
